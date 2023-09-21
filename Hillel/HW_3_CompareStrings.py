bf87d0b2a69f603c7606845bec62010159174136
OlehTsymbaliuk <oleh.tsymbaliuk.work@gmail.com>
2023-09-21 15:17:17 +0300

    * Не зрозумів з першого разу завдання :)

--- Hillel/HW_3_CompareStrings.py
+++ Hillel/HW_3_CompareStrings.py
@@ -1,6 +1,3 @@
 stringA = input('Input the first string:')
 stringB = input('Input the second string:')
-if len(stringA) == len(stringB):
-    print('The lenght of the strings is the same')
-else:
-    print('The lenght of the strings isn\'t the same ')
+print(f'Strings are the same?: {stringB == stringA}')
